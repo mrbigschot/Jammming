e1f5b7b83a232c5b67c254c4e7613fbe96056d68
Paul Schot <pks5885@gmail.com>
2023-07-14 15:53:36 -0700

    Updates

---
+++ precache-manifest.a09725fe1ce6c2902c1999f2de7f6830.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "901f1e37877ee527831f",
+    "url": "/Jammming/static/css/main.cd28c091.chunk.css"
+  },
+  {
+    "revision": "901f1e37877ee527831f",
+    "url": "/Jammming/static/js/main.04a9af9d.chunk.js"
+  },
+  {
+    "revision": "f74819f1b4d06bc5cb44",
+    "url": "/Jammming/static/js/runtime~main.1df0f6e1.js"
+  },
+  {
+    "revision": "4f8549dffa947a35abb9",
+    "url": "/Jammming/static/js/2.b1c4f61e.chunk.js"
+  },
+  {
+    "revision": "6885d93491a5f1f9f154",
+    "url": "/Jammming/static/js/3.4f83f070.chunk.js"
+  },
+  {
+    "revision": "7d9bdb316211a281aa379b888c93079b",
+    "url": "/Jammming/index.html"
+  }
+];
